e82703473d11cbf9e0cbea7ab161406663331739
m4shishi <bloom.cyberix@gmail.com>
2023-09-03 16:31:07 +0400

    1 more

--- flask_test_server.py
+++ flask_test_server.py
@@ -3,8 +3,6 @@ from flask import Flask
 app = Flask("my app")
 
 NETTOOLKIT_API_TOKEN = "test_8F6P51ZjL21CLncoXsJ6Yw1SvOCSe7igfxUaPK7C"
-aws_access_key_id = AKIAYVP4CIPPERUVIFXG
-aws_secret_access_key = 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
 
 @app.route('/')
 def hello():
