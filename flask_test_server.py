7df12a9a3d1191a5c4606e163d3141e49cb865d0
m4shishi <bloom.cyberix@gmail.com>
2023-09-03 13:50:58 +0400

    for trufflehog tests

--- flask_test_server.py
+++ flask_test_server.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask("my app")
 
-NETLIFY_API_TOKEN = "56464-67676-67699"
+NETTOOLKIT_API_TOKEN = "test_8F6P51ZjL21CLncoXsJ6Yw1SvOCSe7igfxUaPK7C"
 
 @app.route('/')
 def hello():
