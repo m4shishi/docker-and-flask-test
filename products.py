00c422cd1116c8ef7a9c009230cfed02a79c8f5f
m4shishi <bloom.cyberix@gmail.com>
2023-09-03 16:12:01 +0400

    1 more

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from werkzeug.exceptions import HTTPException
 #from flask_swagger import swagger
 
+
 products = [
     {'id': 143, 'name': 'Notebook', 'price': 5.49},
     {'id': 144, 'name': 'Black Marker', 'price': 1.99}
